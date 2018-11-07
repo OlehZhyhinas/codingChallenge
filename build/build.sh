bd61c0022a630d0ffacf80c2c3c158c71aea1c78
Oleh Zhyhinas <oleh.zhyhinas1@ibm.com>
2018-11-06 22:41:48 -0500

    build

---
+++ build/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CUR_DIR=$(cd $(dirname $0); pwd 2>/dev/null)
+
+#compile the java files
+javac ${CUR_DIR}/../src/*.java
+#create jar files
+jar -cfve app.jar main ${CUR_DIR}/../src/*.class
+
+tag=`date +%s%3N`
+docker build -t olehsmicroservice:${tag} .
