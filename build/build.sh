27892d0abd1f4f19c5afc01544724d2fe8ead731
Oleh Zhyhinas <oleh.zhyhinas1@ibm.com>
2018-11-06 23:55:36 -0500

    jar changes

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,7 @@ CUR_DIR=$(cd $(dirname $0); pwd 2>/dev/null)
 #compile the java files
 javac ${CUR_DIR}/../src/*.java
 #create jar files
-jar -cfve app.jar Main ${CUR_DIR}/../src/*.class
+jar -cfveM MANIFEST.MF app.jar ${CUR_DIR}/../src/*.class
 chmod 777 app.jar
 
 tag=`date +%s%3N`
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+package src;
+
 public class Main {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
